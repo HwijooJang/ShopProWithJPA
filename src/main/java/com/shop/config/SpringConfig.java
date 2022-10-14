c72871809b5b124d7b7473d4b9ebec99062ce7ba
HwijooJang <jhj9469@naver.com>
2022-10-14 17:04:57 +0900

    로그인 구현

--- src/main/java/com/shop/config/SpringConfig.java
+++ src/main/java/com/shop/config/SpringConfig.java
@@ -1,20 +1,36 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity // 스프링시큐리티필터체인이 자동으로 포함된다.
 public class SpringConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception { //http 요청에 대한 보안을 설정한다.
-
+        http
+                .formLogin()
+                .loginPage("/members/login") // 로그인 페이지 설정
+                .defaultSuccessUrl("/") // 로그인 성공시 이동할 url 설정
+                .usernameParameter("email") // 로그인 시 사용할 파라미터 이름으로 email로 지정
+                .failureUrl("/members/login/error") // 로그인 실패 시 이동할 URL 설정
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 로그아웃 URL 설정
+                .logoutSuccessUrl("/"); // 로그아웃 성공 시 이동할 URL 설정
     }
 
     @Bean
@@ -22,4 +38,10 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    // SpringSecurity에서 인증은 Authentication 으로 이루어지며 AuthenticationManagerBuilder 가 AuthenticationManager를 생성한다.
+        // UserDetailService를 구현하고 있는 객체로 memberService를 지정해주며, 비밀번호 암호화를 위해 passwordEncoder를 지정해준다.
+    }
 }
--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -1,15 +1,23 @@
 package com.shop.service;
 
 import com.shop.entity.Member;
+import com.shop.entity.QItem;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.constraints.Email;
 
 @Service
 @Transactional
 @RequiredArgsConstructor // final이나 @Notnull 이 붙은 생성자를 생성해준다.
-public class MemberService {
+public class MemberService implements UserDetailsService {
+                                        // UserDetailService는 DB에서 회원정보를 가져오는 역할을 담당한다.
     private final MemberRepository memberRepository;
 
     public Member saveMember(Member member){
@@ -24,4 +32,17 @@ public class MemberService {
         }
     }
 
+    @Override //UserDetailService를 사용하면 implement되며 회원정보를 조회하여 사용자의 정보와 권한을 갖는 UserDetails 인터페이스를 반환한다.
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email); // 로그인할 유저의 email을 파라미터로 전달받는다.
+
+        if(member == null){
+            throw new UsernameNotFoundException(email); // 예외처리문
+        }
+        return User.builder() // User 생성을 위해 회원의 이메일, 비밀번호, role 을 파라미터로 넘긴다.
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+    }
 }
