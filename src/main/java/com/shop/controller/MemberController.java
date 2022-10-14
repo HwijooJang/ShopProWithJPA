23bfa0d36124f979fb708fb4cf189f33f3de8251
HwijooJang <jhj9469@naver.com>
2022-10-14 16:14:22 +0900

    validation 이용 유효성 검증

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -25,12 +28,30 @@ public class MemberController {
         return "member/memberForm";
     }
 
-    @PostMapping(value = "/new")
+   /* @PostMapping(value = "/new")
     public String memberForm(MemberFormDto memberFormDto){
 
         Member member = Member.createMember(memberFormDto, passwordEncoder);
         memberService.saveMember(member);
 
+        return "redirect:/";
+    }*/
+
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+                            // 검증하려는 객체안에 @Valid를 선언하고 파라미터로 bindingResult를 추가, 검사 후 결과를 bindingResult에 담고 .hasError를 호출
+                            // 에러가 검증이 되면 회원가입 페이지로 이동한다.
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage()); // 중복회원가입 예외가 발생하면 에러 메시지를 뷰로 전달한다.
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 
