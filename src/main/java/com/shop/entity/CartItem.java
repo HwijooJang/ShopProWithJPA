78ebe2964d46a872fdddf897f7a60370baaeb68f
HwijooJang <jhj9469@naver.com>
2022-10-26 15:05:02 +0900

    cascade, orphanRemoval, fetch 추가

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -16,11 +16,11 @@ public class CartItem {
     @Column(name = "cart_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cart_id")
     private Cart cart; // 하나의 장바구니에는 여러가지 상품을 담을 수 있으므로 @ManyToOne으로 다대일 관계로 매핑
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item; // 장바구니에 담을 상품의 정보를 알아야 하므로 상품 엔티티를 매핑해준다.
                        // 하나의 상품은 여러 장바구니 담길수 있으므로 마찬가지로 @ManyToOne으로 다대일 관계로 매핑한다
--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -20,7 +20,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member; // 한 명의 회원은 여러 번 주문을 할 수 있으므로 주문 엔티티 기준에서 다대일 단반향 매핑을 한다.
 
@@ -30,7 +30,7 @@ public class Order {
     private OrderStatus orderStatus; // 주문상태
 
                                     // cascade : 부모 엔티티의 영속성 상태변화를 자식 엔티티에 모두 전이하는 옵션
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true) // orphanRemoval : 고아객체 제가
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY) // orphanRemoval : 고아객체 제가
     // 주문 상품 엔티티와 일대다 매핑을 한다. 외래키(order_id)가 order_item 테이블에 있으므로 연관 관계의 주인은
     // orderitem 엔티티 이다. Order 엔티티가 주인이 아니므로 "mappedBy" 속성으로 연관 관계의 주인을 설정, 속성 값으로 order를 적어준 이유는 orderItem에 있는
     // Order에 의해 관리된다는 의미로 해석 즉, 연관 관계의 주인의 필드인 order를 mappedBy의 값으로 세팅하면 된다.
