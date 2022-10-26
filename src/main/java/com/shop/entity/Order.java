705617947d956ae271b41d5c93cd785b88824daf
HwijooJang <jhj9469@naver.com>
2022-10-26 14:57:32 +0900

    cascade, orphanRemoval, fetch 테스트

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class Order {
 
     @Id
+    @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
@@ -28,10 +29,11 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus; // 주문상태
 
-    @OneToMany(mappedBy = "order") // 주문 상품 엔티티와 일대다 매핑을 한다. 외래키(order_id)가 order_item 테이블에 있으므로 연관 관계의 주인은
+                                    // cascade : 부모 엔티티의 영속성 상태변화를 자식 엔티티에 모두 전이하는 옵션
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true) // orphanRemoval : 고아객체 제가
+    // 주문 상품 엔티티와 일대다 매핑을 한다. 외래키(order_id)가 order_item 테이블에 있으므로 연관 관계의 주인은
     // orderitem 엔티티 이다. Order 엔티티가 주인이 아니므로 "mappedBy" 속성으로 연관 관계의 주인을 설정, 속성 값으로 order를 적어준 이유는 orderItem에 있는
     // Order에 의해 관리된다는 의미로 해석 즉, 연관 관계의 주인의 필드인 order를 mappedBy의 값으로 세팅하면 된다.
-
     private List<OrderItem> orderItems = new ArrayList<>(); // 하나의 상품이 여러 개의 주문 상품을 갖으므로 List 자료형을 사용해서 매핑
 
     private LocalDateTime regTime;
--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -2,6 +2,7 @@ package com.shop.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.engine.profile.Fetch;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -16,11 +17,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item; // 하나의 상품은 여러 주문 상품으로 들어갈 수 있으므로 주문 상품 기준으로 다대일 단방향 매핑을 설정한다.
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order; // 한 번의 주문엔 여러 개의 상품을 주문할 수 있으므로 주문 상품 엔티티와 주문 엔티티를 다대일 단방향 매핑을 먼저 설정한다.
 
