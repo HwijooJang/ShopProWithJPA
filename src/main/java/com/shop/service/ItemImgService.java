488f4b5f7307dc91024481ef65f865e46b4236a3
HwijooJang <jhj9469@naver.com>
2022-10-30 20:45:05 +0900

    상품(이미지포함) 수정 로직

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -39,8 +41,25 @@ public class ItemImgService {
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
         // 입력받은 상품 이미지의 정보를 저장
+    }
 
-
+    // 상품 이미지 수정
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception {
+        if(!itemImgFile.isEmpty()){ // 상품 이미지를 수정한 경우 상품 이미지를 업데이트
+                                                        // 상품 이미지 아이디를 이용하여 기존에 저장했던 상품 이미지 엔티티를 조회
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+            // 기존 이미지 파일 삭제
+            if(!StringUtils.isEmpty(savedItemImg.getImgName())) { // 기존에 등록된 상품 이미지 파일이 있을 경우 해당 파일을 삭제
+                fileSerivce.deleteFile(itemImgLocation + "/" + savedItemImg.getImgName());
+            }
+            String oriImgName = itemImgFile.getOriginalFilename();
+            String imgName = fileSerivce.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes()); // 업데이트한 상품 이미지 파일을 업로드
+            String imgUrl = "/images/item/" + imgName;
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+            // 변경된 상품 이미지 정보를 세팅해준다. 여기서 중요한 점은 상품 등록 때처럼 itemImgRepository.save() 로직을 호출하지 않는 다는 것이다.
+            // savedItemImg 엔티티는 현재 영속 상태이므로 데이터를 변경하는 것만으로 변경 감지 기능이 동작하여 트랜잭션이 끝날때 update 쿼리가 실행된다.
+            // 여기서 중요한 것은 엔티티가 영속 상태 여야 한다는 점이다.
+        }
     }
 
 
